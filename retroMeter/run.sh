723abf6b1e716244a422b5b2cb7305d75fea9182
Dominik Widmann <dominik.widmann@live.de>
2022-11-17 00:07:47 +0100

    add mqtt config

--- retroMeter/run.sh
+++ retroMeter/run.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
+echo "Starting retrometer add on..."
 
-python3 measure_gas.py
+# Declare variables
+declare mqttuser
+declare mqttpasswd
+
+## Get the 'mqttuser' key from the user config options.
+mqttuser=$(bashio::config 'mqttuser')
+mqttpasswd=$(bashio::config 'mqttpasswd')
+
+## Print the config the user supplied"
+bashio::log.info "Mqtt user: ${mqttuser}, passwd: ${mqttpasswd}"
+
+python3 /retrometer/measure_gas.py --mqttuser ${mqttuser} --mqttpasswd ${mqttpasswd} -v
