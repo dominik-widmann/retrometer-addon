96c9e528327e0189317ed54082f8861396f93fe0
Dominik Widmann <dominik.widmann@live.de>
2022-11-17 18:46:00 +0100

    Add some configuration parameters

--- retroMeter/retroMeter/README.md
+++ retroMeter/retroMeter/README.md
@@ -6,4 +6,5 @@
 - added mqtt sensor for volume and template sensor for kWh in home assistant energy configuration
 - enabled i2c via the haas i2c configurator add-on (protection disabled)
 - configure mqtt credentials
+- enable mqtt integration with mosquito
 - setup dashboard:
--- retroMeter/run.sh
+++ retroMeter/run.sh
@@ -5,9 +5,14 @@ echo "Starting retrometer add on..."
 # Declare variables
 declare mqttuser
 declare mqttpasswd
+declare mqtttopic
+declare m3pertick
+
 
 ## Get the 'mqttuser' key from the user config options.
 mqttuser=$(bashio::config 'mqttuser')
 mqttpasswd=$(bashio::config 'mqttpasswd')
+mqtttopic=$(bashio::config 'mqtttopic')
+m3pertick=$(bashio::config 'm3pertick')
 
-python3 /retrometer/measure_gas.py --mqttuser ${mqttuser} --mqttpasswd ${mqttpasswd} -v
+python3 /retrometer/measure_gas.py --mqttuser ${mqttuser} --mqttpasswd ${mqttpasswd} --mqtttopic ${mqtttopic} --m3pertick ${m3pertick} -v
